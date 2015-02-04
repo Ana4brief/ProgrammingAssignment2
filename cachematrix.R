50b211953900cd5a8240877c7e85640ae0bfe7dd
Ana4brief <anapascal.research@gmail.com>
2015-02-04 17:20:33 +0000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+## speeds up calculating a matrix inverse using scoping rules and cache
 
-## Write a short comment describing this function
+## list of functions to set/get a matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL
+    set <- function(y){
+    	x<<-y
+	inverse<<-NULL
+    	}
+    get <- function() x
+    setinverse <- function(inv) inverse <<-inv
+    getinverse <- function() inverse
+    list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
-
-## Write a short comment describing this function
+## looks for inverse in cache, if not found computes is
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	inverse <- x$getinverse()
+	if(!is.null(inverse)) {
+	    message("getting cached data")
+	    return(inverse)
+	}
+	data<-x$get()
+	inverse <-solve(data)
+	x$setinverse(inverse)
+	inverse
 }
